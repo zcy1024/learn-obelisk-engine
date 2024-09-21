552163f4a26cee9c7ce43565f05ca84d7fae7f3c
Zcy <zcy_chao1024@163.com>
2024-09-21 11:47:33 +0800

    world-publish && storeConfig

--- Blackjack/src/chain/config.ts
+++ Blackjack/src/chain/config.ts
@@ -2,8 +2,8 @@ type NetworkType = 'testnet' | 'mainnet' | 'devnet' | 'localnet';
 
 const NETWORK: NetworkType = 'testnet';
 
-const PACKAGE_ID = '0x0b4e7ba2a2d98a3f5b8a24d794ef4993244f880e9419e83ffb6a1acb81e1e476'
-const WORLD_ID = '0x050a096a3864de6d3c6098f1837ad3a49964e21a2d32d3bb58db49ce3ae496fe'
+const PACKAGE_ID = '0x038cf8c26bbb3199a40e58e47a65a4f63abaa506fd8b2ef1057de3690aed80be'
+const WORLD_ID = '0x6f234410561d578881d859bda3f5da09eef40cd0df2aed22cdbfe46de35d3611'
 
 export {
     NETWORK,
