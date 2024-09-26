c41702723a4cd29d4932aca97f78776423902ade
Zcy <zcy_chao1024@163.com>
2024-09-26 22:12:06 +0800

    Project and pre-data initialization

---
+++ Suikemon/src/chain/config.ts
@@ -0,0 +1,12 @@
+type NetworkType = 'testnet' | 'mainnet' | 'devnet' | 'localnet';
+
+const NETWORK: NetworkType = 'testnet';
+
+const PACKAGE_ID = '0x2baef61bdb7dbd824ca08e8f928909a726f11448ade60436b6b4e0433db3e72e'
+const WORLD_ID = '0xf41a1c814cc3054d18e3da7271bb48824dbe79bc1989161be3c5947c7f9ce1a9'
+
+export {
+    NETWORK,
+    PACKAGE_ID,
+    WORLD_ID,
+}
---
+++ Suikemon/src/data/filter.ts
@@ -0,0 +1,36 @@
+import * as fs from 'fs'
+
+const origin = fs.readFileSync("./originData.txt", {encoding: "utf-8", flag: 'r'})
+
+const step1 = origin.split("\n")
+
+const step2 = step1.filter(str => str.length > 2 && str.split('|')[0] === "{{Rdexn")
+
+// step3 to resolve: 水}} can't be selected
+const step3 = step2.map(str => str.slice(0, str.length - 2))
+
+const step4 = step3.filter(str => {
+    const array = str.split('|')
+    for (let i = 0; i < array.length; i++)
+        if (array[i] === "水")
+            return true
+    return false
+})
+
+const step5 = step4.map(str => str.split('|')[1] + ' ' + str.split('|')[2] + ' ' + str.split('|')[3] + ' ' + str.split('|')[4])
+
+const step6 = step5.map(str => str + " https://wiki.52poke.com/wiki/" + encodeURIComponent(str.split(' ')[1]))
+
+const finalData = step6.map(str => {
+    const idx = str.split(' ')[0]
+    let pre = ""
+    while (pre.length + idx.length < 4)
+        pre += '0'
+    return str + " .sprite-icon-" + pre + idx
+})
+
+// console.log(finalData)
+
+fs.writeFileSync("./data.txt", "// index   chinese   japanese   english   detail   sprite-icon\n" + finalData.join("\n"))
+
+// Finally, manually fine-tune according to the rendering effect
