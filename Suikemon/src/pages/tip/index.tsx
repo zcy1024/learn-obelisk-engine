0d64f423513138ae14e0e1a939f33b8dd169f0f8
Zcy <zcy_chao1024@163.com>
2024-09-28 08:49:52 +0800

    page tip

--- Suikemon/src/pages/tip/index.tsx
+++ Suikemon/src/pages/tip/index.tsx
@@ -1,7 +1,33 @@
-const Tip = () => {
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { useCurrentAccount } from "@mysten/dapp-kit";
+
+const Tip = ({ tips }: { tips: string }) => {
+    const router = useRouter()
+    const account = useCurrentAccount()
+
+    const delay = async (ms: number) => {
+        const sleep = async (ms: number) => {
+            return new Promise(resolve => setTimeout(resolve, ms));
+        }
+        await sleep(ms)
+        router.back()
+    }
+
+    useEffect(() => {
+        if (router.isReady && !account)
+            router.push("/tip/notConnect")
+        else if (router.isReady && account)
+            delay(3000)
+    }, [router, account])
     return (
-        <div>
-            tip
+        <div className="relative w-screen h-screen z-50">
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 tracking-widest">
+                <div className="animate-bounce">
+                    <span className="sprite-icon sprite-icon-000" title="tips"></span>
+                    {tips}
+                </div>
+            </div>
         </div>
     );
 };
--- Suikemon/src/pages/tip/notConnect.tsx
+++ Suikemon/src/pages/tip/notConnect.tsx
@@ -1,16 +1,20 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { useCurrentAccount } from "@mysten/dapp-kit";
+
 const NotConnect = () => {
     const router = useRouter()
     const account = useCurrentAccount()
     useEffect(() => {
         if (router.isReady && account)
-            router.replace("/")
+            router.back()
     }, [router, account])
     return (
-        <div>
-            NotConnect
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 tracking-widest">
+            <div className="animate-bounce">
+                <span className="sprite-icon sprite-icon-000" title="tips"></span>
+                Please Connect Wallet First!
+            </div>
         </div>
     );
 };
